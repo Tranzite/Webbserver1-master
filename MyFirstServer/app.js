5e09169ec9a21cc9552cb01e9b0e86a2bb7a95b1
Tranzite <54800477+Tranzite@users.noreply.github.com>
2020-09-03 12:45:25 +0200

    min första web server
    hej kompis

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -9,5 +9,12 @@ app.get('/style', (req, res) => {
   res.sendFile(clientDir + "index.css")
 })
 
-
+app.get('/cssen', (req, res) => { //Detta är hur man får css att fungera man måste skicka en get request + namn och sen göra sendFile(clientDir och sen + css filen)
+  res.sendFile(clientDir + "index.css")
+})
+app.get('/nightmare', (req, res) => { //Detta är hur man får en bild att fungera det är samma princip som css.
+  res.sendFile(clientDir + "nightmareKingGrimm.png")
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
